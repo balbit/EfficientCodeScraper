97b76aa9f74f6e19b214fd79d9e050a2b92eb81c
balbit <elliotliu123@gmail.com>
2023-01-19 10:29:29 +0800

    Update run.sh
    Signed-off-by: balbit <elliotliu123@gmail.com>

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ if (($# < 1)); then
 fi
 
 echo "0" > down
-d1=$(du -s /media/bo/0123-4567/MTK/got | cut -f1)
+
 
 for i in $(seq 1); do
 echo "Getting List";
@@ -18,7 +18,5 @@ python3 getcontents.py;
 s3=$(date +%s);
 echo $(($s2-$s1))" seconds taken to get list";
 echo $(($s3-$s2))" seconds taken to download";
-d2=$(du -s /media/bo/0123-4567/MTK/got | cut -f1)
-echo $(($d2-$d1))", "$(cat down)": took, got";
 sleep 1;
 done
