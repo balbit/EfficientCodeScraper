16fd71ea8b8a3376e6707b95f85a6f59c7b34799
balbit <elliotliu123@gmail.com>
2023-01-19 10:33:31 +0800

    Update getlist.sh
    Signed-off-by: balbit <elliotliu123@gmail.com>

--- getlist.sh
+++ getlist.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+# getlist.sh downloads information about the 100 most recent directories after $idnum, 
+# which is stored in lastid.txt, and cleans them into repolinks.txt, after which $idnum is updated
+
 idnum=$(cat lastid.txt)
 gh api -H "Accept: application/vnd.github+json" /repositories?since=$idnum > repolist.txt
 python3 cleanrepo.py $1 >> repolinks.txt
