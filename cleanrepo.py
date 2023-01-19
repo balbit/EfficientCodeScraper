feae6090d258b40d36d85af4676814f3edb3d4dc
balbit <elliotliu123@gmail.com>
2023-01-19 10:36:04 +0800

    Update cleanrepo.py
    Signed-off-by: balbit <elliotliu123@gmail.com>

--- cleanrepo.py
+++ cleanrepo.py
@@ -1,3 +1,8 @@
+# cleanrepo.py obtains more information about each of the 100 directories listed 
+# and outputs the information into repolinks.txt
+# The set of important information to be scraped can be edited by modifying the list 
+# imp. 
+
 import json
 import requests
 import sys
@@ -8,8 +13,6 @@ l = json.load(f)
 
 imp = ['id','url','full_name','stargazers_count','language','size']
 
-
-
 if len(sys.argv) > 1 and len(sys.argv[1]) <= 1:
 	print('\t'.join(imp + ['license']))
 	sys.exit()
@@ -18,10 +21,8 @@ tok = sys.argv[1] if len(sys.argv)>1 else ''
 
 
 for e in l:
-#	print(e['url'])
 	try:
 		info = requests.get(e['url'], headers={'Authorization':'Bearer '+tok}).json()
-#		print(info)
 		li = 'null'
 		try:
 			li = info['license']['key']
